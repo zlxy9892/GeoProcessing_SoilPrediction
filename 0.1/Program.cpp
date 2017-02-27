f5a09dc228328d773fcc23ff88075996c5a81d55
Jerry <Jerry>
2017-02-27 10:50:49 +0800

    delete something in 'Program.cpp' file.

--- 0.1/Program.cpp
+++ 0.1/Program.cpp
@@ -6,35 +6,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout<<"\nInitalizing...\n";
-	
-	// 处理输入参数
-
-	// 示例
-	argv[1] = "E:/data/samplegroup/geo.asc#E:/data/samplegroup/slope.asc#E:/data/samplegroup/planc.asc";	// 输入的环境因子图层
-	argv[2] = "category?boolean#category?gower#category?gower";												// 环境因子计算类型
-	argv[3] = "ID";																							// 样点ID在输入表格中的表头名称
-	argv[4] = "shali";																						// 样点目标属性变量在输入表格中的表头名称
-	argv[5] = "0.95";																						// 环境相似度阈值
-	argv[6] = "10";																							// 土壤属性宽度阈值
-	argv[7] = "E:/data/samplegroup/samples.csv";															// 输入的样点数据（包含ID，x，y，土壤属性）
-	argv[8] = "E:/data/samplegroup/result.csv";																// 输出样点可信度结果文件
-	//string params = "";
-
-	/*if(argc != 9)
-	{
-		cout<<"输入参数有误！\n";
-		return 999;
-	}*/
-	char *str_inputEnvLayerFilenames = argv[1];
-	char *str_cate_calMethod = argv[2];
-	char *str_idName = argv[3];
-	char *str_targetVName = argv[4];
-	char *str_threshold_envSimi = argv[5];
-	char *str_threshold_targetVDist = argv[6];
-	char *str_samplesCSV = argv[7];
-	char *str_resultCSV = argv[8];
-
 	// 初始化环境因子数据
 	GDALAllRegister();
 	EnvDataset *envDataset = new EnvDataset();
